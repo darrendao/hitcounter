daccaf139fbfaded6d0825fbc9093251dd488124
Darren Dao <darren.dao@jpl.nasa.gov>
2015-08-16 08:20:05 -0700

    Use Text instead of String for url field

--- lib/hit.rb
+++ lib/hit.rb
@@ -5,7 +5,7 @@ require 'data_mapper'
 class Hit
   include DataMapper::Resource
   property :id, Serial
-  property :url, String, required: true
+  property :url, Text, required: true
   property :ip_address, String, required: true
   property :created_at, DateTime
 end
